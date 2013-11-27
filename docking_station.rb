2976e2117268648ad3d9a06a773c23d4e4c292d0
TomGroombridge <groombridgetom@gmail.com>
2013-11-27 18:09:42 +0000

    working

--- docking_station.rb
+++ docking_station.rb
@@ -1,35 +1,18 @@
+
+
 class DockingStation
+   include BikeContainer
+	
   DEFAULT_CAPACITY = 20
 
   def initialize(options = {})
-  	@capacity = options.fetch(:capacity) || DEFAULT_CAPACITY
-	@bikes = []
+  	@capacity = options.fetch(:capacity, DEFAULT_CAPACITY) 
   end
+  
 
-  def bike_count
-    @bikes.count
-  end
-
-  def dock (bike)
-  	raise "station is full" if full?
-	@bikes << bike
-  end	
-
-  def release (bike)
-	@bikes.delete(bike)
-  end
-
-  def full?
-    bike_count == @capacity
-  end
-
-  def working_bikes
-  	@bikes.select {|bike| bike.broken? }
-  end
-
-  def available_bikes
-  	@working_bikes = working_bikes
-  end
 end
+  
+ 
+
 
 
